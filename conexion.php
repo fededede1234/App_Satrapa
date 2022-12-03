7f75ca8172d2ca3e1d29e0d9540b579dd193f9a4
fededede1234 <119745065+fededede1234@users.noreply.github.com>
2022-12-03 11:37:55 -0300

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,5 @@
+<?php
+$conexion = mysqli_connect('localhost', 'id19064881_fede', '|ze|J<cQ4Y2D=sG\', 'id19064881_app_db')
+	or die("No se pudo conectar");
+mysqli_set_charset($conexion, "utf8");
+?>
